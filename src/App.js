64ec5e262bd9a07db2122c7040dffc98260e59c7
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-06-27 14:15:49 +1000

    Rendering checkbox component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Checkbox from './components/Checkbox'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>  test </h1>
+        <h1> Hata la Lista </h1>
+        <Checkbox label={ 'label!' } checked={ true } />
       </div>
     );
   }
---
+++ src/components/Checkbox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Checkbox({
+  checked,
+  label
+}) {
+  return (
+      <label>
+        <input type="checkbox" checked={ checked } />
+        { ' ' }
+        { label } 
+      </label>
+  )
+}
